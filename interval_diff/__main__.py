ab00ae1722f259b6cad44d710cc3d55145e68cfa
BlakeJC94 <blakejamescook@gmail.com>
2022-12-26 15:55:01 +1100

    feat: main

---
+++ interval_diff/__main__.py
@@ -0,0 +1,29 @@
+import argparse
+import sys
+
+from .benchmark import benchmark
+
+def parse_cli_input():
+    parser = argparse.ArgumentParser("interval-diff", add_help=True)
+    parser.add_argument(
+        "--n-intervals",
+        "-n",
+        nargs="*",
+        type=int,
+        help="number of intervals to run algorithms in each sample."
+    )
+    parser.add_argument(
+        "--n-samples",
+        "-k",
+        nargs="?",
+        type=int,
+        help="number of random samples to run algorithms."
+    )
+
+    return parser.parse_args()
+
+def main():
+    benchmark(**vars(parse_cli_input()))
+
+if __name__ == "__main__":
+    sys.exit(main())
