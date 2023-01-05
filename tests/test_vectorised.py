a82db828b74ee07a05454b69df9b7a61a9690e2b
BlakeJC94 <blakejamescook@gmail.com>
2023-01-05 16:53:26 +1100

    Rfc tests (#2)
    * test: results_equal helper
    
    * test: parameterise df vectorised
    
    * test: add df test for docs
    
    * test: test_some_partially_overlapping
    
    * rfc: test cases as strings
    
    * rfc: add method to test
    
    * docs: update futures

--- tests/test_vectorised.py
+++ tests/test_vectorised.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import pandas as pd
 
 from interval_diff.vectorised import (
     interval_difference,
@@ -7,229 +8,304 @@ from interval_diff.vectorised import (
 )
 
 
+
+
+@pytest.mark.parametrize("df", [False, True])
 class TestIntervalDifference:
-    intervals_a = np.array([(100, 200), (600, 700), (1100, 1200), (2000, 2200)])
 
-    # A     : (----)  (----)  (----)  (----)         (----) (------)
-    # B     :    (---------------)      (------)  (----)      (----)
-    # A \ B : (--)               (-)  (-)              (--) ()
-    def test_doc_example(self):
-        intervals_a = np.array(
-            [(100, 200), (300, 400), (500, 600), (700, 800), (1000, 1100), (1250, 1400)]
+    @staticmethod
+    def parse_intervals(records, df: bool):
+        if isinstance(records, str):
+            starts, ends, tags = [], [], []
+            default_tag = "-"
+            for i, c in enumerate(records):
+                if c == "(":
+                    starts.append(i * 100)
+                    tags.append(records[i + 1] if records[i + 1].isalpha() else default_tag)
+                elif c == ")":
+                    ends.append(i * 100)
+
+            if df:
+                records = list(zip(starts, ends, tags))
+            else:
+                records = list(zip(starts, ends))
+
+        out = None
+        if df:
+            if len(records) > 0 and len(records[0]) == 2:
+                records = [(*r, "q") for r in records]
+            columns = ["start", "end", "tags"]
+            out = pd.DataFrame(records, columns=columns)
+        else:
+            out = np.array([(r[0], r[1]) for r in records])
+            if len(out) == 0:
+                out = np.empty((0,2))
+
+        return out
+
+
+    @staticmethod
+    def results_equal(output, expected):
+        if type(output) != type(expected):
+            raise TypeError()
+        if not isinstance(output, (pd.DataFrame, np.ndarray)):
+            raise ValueError()
+
+        if isinstance(output, pd.DataFrame):
+            if output.empty or expected.empty:
+                return (output.columns == expected.columns).all()
+
+            return output.equals(expected)
+
+        return np.array_equal(output, expected)
+
+    def test_doc_example(self, df):
+        intervals_a, intervals_b, expected = (
+            " (q---)  (w---)  (e---)  (r---)         (t---) (y-----)",
+            "    (---------------)      (------)  (----)      (----)",
+            " (q-)               (e)  (r)              (t-) (y)",
         )
-        intervals_b = np.array([(150, 580), (720, 890), (930, 1070), (1300, 1400)])
-        expected = np.array([(100, 150), (580, 600), (700, 720), (1070, 1100), (1250, 1300)])
-        result = interval_difference(intervals_a, intervals_b)
-        assert np.array_equal(result, expected)
 
-    # PASS
+        intervals_a = self.parse_intervals(intervals_a, df=df)
+        intervals_b = self.parse_intervals(intervals_b, df=df)
+        expected = self.parse_intervals(expected, df=df)
+
+        result = interval_difference(intervals_a, intervals_b)
+
+        assert self.results_equal(result, expected)
+
     @pytest.mark.parametrize(
-        "intervals_b, expected",
+        "intervals_a, intervals_b, expected",
         [
             # 2 left partial overlaps
             (
-                np.array([(80, 120), (580, 620)]),
-                np.array(
-                    [
-                        (120, 200),
-                        (620, 700),
-                        (1100, 1200),
-                        (2000, 2200),
-                    ]
-                ),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (--)     (--)                             ",
+                "    (q-)     (w--)    (e---)        (e----)",
             ),
             # 2 right partial overlaps
             (
-                np.array([(180, 220), (680, 720)]),
-                np.array(
-                    [
-                        (100, 180),
-                        (600, 680),
-                        (1100, 1200),
-                        (2000, 2200),
-                    ]
-                ),
+                "  (q---)    (w---)    (e---)        (e----)",
+                "     (--)     (--)                         ",
+                "  (q-)      (w)       (e---)        (e----)",
             ),
             # 1 left, 1 right partial overlaps
             (
-                np.array([(80, 120), (680, 720)]),
-                np.array(
-                    [
-                        (120, 200),
-                        (600, 680),
-                        (1100, 1200),
-                        (2000, 2200),
-                    ]
-                ),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (--)         (--)                         ",
+                "    (q-)    (w)       (e---)        (e----)",
             ),
         ],
     )
-    def test_some_partially_overlapping(self, intervals_b, expected):
-        result = interval_difference(self.intervals_a, intervals_b)
-        assert np.array_equal(expected, result)
+    def test_some_partially_overlapping(self, intervals_a, intervals_b, expected, df):
+        intervals_a = self.parse_intervals(intervals_a, df=df)
+        intervals_b = self.parse_intervals(intervals_b, df=df)
+        expected = self.parse_intervals(expected, df=df)
 
-    # PASS
+        result = interval_difference(intervals_a, intervals_b)
+        assert self.results_equal(expected, result)
+
+    intervals_a = [(100, 200, "q"), (600, 700, "w"), (1100, 1200, "e"), (2000, 2200, "e")]
     @pytest.mark.parametrize(
-        "intervals_b, expected",
+        "intervals_a, intervals_b, expected",
         [
             # 1 total overlap
             (
-                np.array([(80, 220)]),
-                np.array([(600, 700), (1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (------)                                  ",
+                "            (w---)    (e---)        (e----)",
             ),
             # 1 total, 1 left
             (
-                np.array([(80, 220), (580, 620)]),
-                np.array([(620, 700), (1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (------)  (--)                            ",
+                "              (w-)    (e---)        (e----)",
             ),
             # 1 left, 1 total
             (
-                np.array([(80, 120), (580, 720)]),
-                np.array([(120, 200), (1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (--)     (--------)                       ",
+                "    (q-)              (e---)        (e----)",
             ),
             # 1 total, 1 right
             (
-                np.array([(80, 220), (680, 720)]),
-                np.array([(600, 680), (1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (------) (---)                            ",
+                "              (w-)    (e---)        (e----)",
             ),
             # 1 right, 1 total
             (
-                np.array([(180, 220), (580, 720)]),
-                np.array([(100, 180), (1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                "     (--) (--------)                       ",
+                "  (q-)                (e---)        (e----)",
             ),
             # 2 total
             (
-                np.array([(80, 220), (580, 720)]),
-                np.array([(1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (-------) (--------)                      ",
+                "                      (e---)        (e----)",
             ),
         ],
     )
-    def test_some_totally_overlapping(self, intervals_b, expected):
-        result = interval_difference(self.intervals_a, intervals_b)
-        assert np.array_equal(expected, result)
+    def test_some_totally_overlapping(self, intervals_a, intervals_b, expected, df):
+        intervals_a = self.parse_intervals(intervals_a, df=df)
+        intervals_b = self.parse_intervals(intervals_b, df=df)
+        expected = self.parse_intervals(expected, df=df)
 
-    # PASS
+        result = interval_difference(intervals_a, intervals_b)
+        assert self.results_equal(expected, result)
+
     @pytest.mark.parametrize(
-        "intervals_b",
+        "intervals_a, intervals_b",
         [
             # In between intervals
-            np.array([(300, 400), (500, 550), (800, 900)]),
+            (
+                "  (q---)    (w---)    (e---)        (e----)",
+                "        (--)      (--)       (---)         ",
+            ),
             # All strictly left
-            np.array([(40, 50), (60, 70), (80, 90)]),
+            (
+                "               (q---)    (w---)    (e---)        (e----)",
+                " (--) (--) (--)                                         ",
+            ),
             # All strictly right
-            np.array([(2540, 2550), (2560, 2570), (2580, 2590)]),
+            (
+                " (q---)    (w---)    (e---)        (e----)                ",
+                "                                           (--) (--) (--) ",
+            ),
             # All strictly non-overlapping left and right
-            np.array([(40, 50), (60, 70), (2560, 2570), (2580, 2590)]),
+            (
+                "                (q---)    (w---)    (e---)        (e----)                ",
+                " (--) (--) (--)                                           (--) (--) (--) ",
+            ),
         ],
     )
-    def test_none_overlapping(self, intervals_b):
-        expected = self.intervals_a
-        result = interval_difference(self.intervals_a, intervals_b)
-        assert np.array_equal(expected, result)
+    def test_none_overlapping(self, intervals_a, intervals_b, df):
+        intervals_a = self.parse_intervals(intervals_a, df=df)
+        intervals_b = self.parse_intervals(intervals_b, df=df)
+
+        result = interval_difference(
+            intervals_a,
+            intervals_b,
+        )
+        assert self.results_equal(intervals_a, result)
 
     @pytest.mark.parametrize(
-        "minuend, expected_minuend_result",
+        "minuend, intervals_b, expected",
         [
-            # Minuends have no overlap with subtrahends (return minuend unchanged) PASS
+            # Minuends have no overlap with subtrahends (return minuend unchanged)
             (
-                np.array([(300, 400), (800, 900)]),
-                np.array([(300, 400), (800, 900)]),
+                "        (q-)      (e-)                     ",
+                "  (----)    (----)    (----)        (-----)",
+                "        (q-)      (e-)                     ",
             ),
-            # All minuends overlapped by middle 2 subtrahends (return empty list) PASS
+            # All minuends overlapped by middle 2 subtrahends (return empty)
             (
-                np.array([(610, 690), (1110, 1190)]),
-                np.empty((0, 2)),
+                "             (q-)      (e-)                ",
+                "  (----)    (----)    (----)        (-----)",
+                "                                           ",
             ),
             # All minuends left-overlap first 3 subtrahends (trim minuend_ends)
             (
-                np.array([(50, 150), (550, 650), (1050, 1150)]),
-                np.array([(50, 100), (550, 600), (1050, 1100)]),
+                "(q--)     (e--)     (e--)                  ",
+                "  (----)    (----)    (----)        (-----)",
+                "(q)       (e)       (e)                    ",
             ),
             # All minuends right-overlap first 3 subtrahends (trim minuend_starts)
             (
-                np.array([(150, 250), (650, 750), (1150, 1250)]),
-                np.array([(200, 250), (700, 750), (1200, 1250)]),
+                "     (q--)     (e--)     (e--)             ",
+                "  (----)    (----)    (----)        (-----)",
+                "       (q)       (e)       (e)             ",
             ),
             # All minuends overlap first 2 subtrahends (split minuends and trim)
             (
-                np.array([(90, 210), (590, 710)]),
-                np.array([(90, 100), (200, 210), (590, 600), (700, 710)]),
+                " (q-------)(e-------)                       ",
+                "   (----)    (----)    (----)        (-----)",
+                " (q)    (q)(e)    (e)                       ",
             ),
             # All minuends overlap multiple subtrahends (split minuends and trim)
             (
-                np.array([(90, 710), (1050, 2250)]),
-                np.array(
-                    [
-                        (90, 100),
-                        (200, 600),
-                        (700, 710),
-                        (1050, 1100),
-                        (1200, 2000),
-                        (2200, 2250),
-                    ]
-                ),
+                " (q-----------------)(e-----------------------) ",
+                "   (----)    (----)    (----)        (-----)    ",
+                " (q)    (q---)    (q)(e)    (e-------)     (e-) ",
             ),
         ],
     )
     def test_interval_difference_minuend(
         self,
         minuend,
-        expected_minuend_result,
+        intervals_b,
+        expected,
+        df,
     ):
-        output = interval_difference(minuend, self.intervals_a)
-        assert np.array_equal(expected_minuend_result, output)
+        minuend = self.parse_intervals(minuend, df=df)
+        intervals_b = self.parse_intervals(intervals_b, df=df)
+        expected = self.parse_intervals(expected, df=df)
 
+        output = interval_difference(
+            minuend,
+            intervals_b
+        )
+        assert self.results_equal(expected, output)
+
+    intervals_a = [(100, 200, "q"), (600, 700, "w"), (1100, 1200, "e"), (2000, 2200, "e")]
     @pytest.mark.parametrize(
-        "subtrahend, expected_subtrahend_result",
+        "intervals_a, subtrahend, expected",
         [
             # Subtrahends don't overlap with minuends (return minuend unchanged)
             (
-                np.array([(300, 400), (800, 900)]),
-                np.array([(100, 200), (600, 700), (1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                "        (--)      (--)                     ",
+                "  (q---)    (w---)    (e---)        (e----)",
             ),
             # Middle 2 minuends overlap all subtrahends (trim and split minuends, return non-overlap)
             (
-                np.array([(610, 690), (1110, 1190)]),
-                np.array(
-                    [
-                        (100, 200),
-                        (600, 610),
-                        (690, 700),
-                        (1100, 1110),
-                        (1190, 1200),
-                        (2000, 2200),
-                    ]
-                ),
+                "  (q---)    (w---)    (e---)        (e----)",
+                "              ()        ()                 ",
+                "  (q---)    (w)(w)    (e)(e)        (e----)",
             ),
             # First 3 minuends left-overlap all subtrahends (trim minuend_ends, return non-overlap)
             (
-                np.array([(50, 150), (550, 650), (1050, 1150)]),
-                np.array([(150, 200), (650, 700), (1150, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (---)     (---)     (---)                 ",
+                "     (q)       (w)       (e)        (e----)",
             ),
             # First 3 minuends right-overlap all subtrahends (trim minuend_starts, return non-overlap)
             (
-                np.array([(150, 250), (650, 750), (1150, 1250)]),
-                np.array([(100, 150), (600, 650), (1100, 1150), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                "    (---)     (---)     (---)              ",
+                "  (q)       (w)       (e)           (e----)",
             ),
             # First 2 minuends overlapped by subtrahends (drop overlapped minuends, return non-overlap)
             (
-                np.array([(90, 210), (590, 710)]),
-                np.array([(1100, 1200), (2000, 2200)]),
+                "  (q---)    (w---)    (e---)        (e----)",
+                " (------)  (------)                        ",
+                "                      (e---)        (e----)",
             ),
             # All minuends overlapped by 2 subtrahends (drop all minuends)
             (
-                np.array([(90, 710), (1050, 2250)]),
-                np.empty((0, 2)),
+                "  (q---)    (w---)    (e---)        (e----) ",
+                " (----------------)  (---------------------)",
+                "                                            ",
             ),
         ],
     )
     def test_interval_difference_subtrahend(
         self,
+        intervals_a,
         subtrahend,
-        expected_subtrahend_result,
+        expected,
+        df,
     ):
-        output = interval_difference(self.intervals_a, subtrahend)
-        assert np.array_equal(expected_subtrahend_result, output)
+        intervals_a = self.parse_intervals(intervals_a, df=df)
+        subtrahend = self.parse_intervals(subtrahend, df=df)
+        expected = self.parse_intervals(expected, df=df)
+
+        output = interval_difference(
+            intervals_a,
+            subtrahend,
+        )
+        assert self.results_equal(expected, output)
 
 
 def test_sort_intervals_by_start():
